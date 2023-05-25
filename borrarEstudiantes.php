14f4e968686a258ef7bf9df359ccba0c2ee1751f
AsynkDF <mrlgsus22@gmail.com>
2023-05-25 10:12:52 -0500

    feat: :tada: Mysql db, php cruds
    cruds con bases de datos

---
+++ borrarEstudiantes.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("config.php");
+    $record = new Config();
+    
+    if (isset($_GET['id']) && isset($_GET['req'])){
+        if ($_GET['req'] == "delete"){
+            $record -> setId($_GET['id']);
+            $record -> delete();
+            echo "<script> alert('Datos Borrados Exitosamente'); document.location='estudiantes.php'</script>";
+        }
+    }
+?>
